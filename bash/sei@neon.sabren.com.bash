87ce27350432f76d38a2fdab865cbca1058a263a
tangentstorm <michal.wallace@gmail.com>
2012-12-23 23:41:07 -0500

    bash settings for sei@neon account

--- bash/sei@neon.sabren.com.bash
+++ bash/sei@neon.sabren.com.bash
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+# User specific environment and startup programs
+export MAIL=
+export MAILCHECK=999999999999
+alias h="clear ; ~/bin/oldest.py -h"
+
 PS1="$BLUE[$WHITE\w$BLUE]$RESET$CYAN:$RESET "
+PATH=$HOME/bin:$HOME/sei/bin:$PATH
+PYTHONPATH=.:~/lib/workshop/code:~/lib
+LANG=en_US
