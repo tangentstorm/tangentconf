a0808f04ed5398a968ee6a9385308268908b0d86
tangentstorm <michal.wallace@gmail.com>
2013-03-03 16:41:09 -0600

    fix compiler executable name for setfpc

--- bin/setfpc.py
+++ bin/setfpc.py
@@ -5,9 +5,9 @@ program to configure which version of free pascal gets run.
 import os, sys
 
 if len(sys.argv) == 2 and sys.argv[1] in ['custom', 'system']:
-    for app, path in [('fpc', 'compiler'), ('fp', 'ide' )]:
+    for app, path in [('fpc', 'compiler/ppcx64'), ('fp', 'ide/fp' )]:
         symlink = os.sep.join(['~', 'bin', app])
         os.system('rm -f ' + symlink)
         if sys.argv[1] == 'custom':
-            os.system('ln -s ~/f/{0}/{1} {2}'.format(path, app, symlink))
+            os.system('ln -s ~/f/{0} {1}'.format(path, symlink))
 else: print "usage : setfpc.py (custom|system)"
