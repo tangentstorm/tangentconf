7bce00753a0f04eecdd3d6745e3d7aef052c506a
tangentstorm <michal.wallace@gmail.com>
2013-08-11 12:00:00 -0500

    old script to merge my retro mirror

---
+++ bin/merge-retro.sh
@@ -0,0 +1,8 @@
+# retro uses launchpad/bzr and github won't accept the official
+# retro history because of a mal-formed address.
+#
+# i don't have a good answer to this at the moment, but it's
+# easy enough to just maintain a snapshot
+
+# git clone https://code.google.com/p/retro-language/ 
+# rsync -avd --delete --exclude .git retro-language/ ~/vrx
