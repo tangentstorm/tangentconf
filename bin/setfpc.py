bccf3a40619a6157dd0be28491b0903eabac9f8e
tangentstorm <michal.wallace@gmail.com>
2013-09-24 16:00:00 -0500

    bin: makefcp and setfpc from last time i had to recompile

--- bin/setfpc.py
+++ bin/setfpc.py
@@ -1,16 +1,17 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 """
 program to configure which version of free pascal to run next.
 """
 import os, sys
 
+platform = "x86_64-freebsd"
 menu = {
   'custom': { 'fpc' : '~/f/compiler/ppcx64',
-              'fp'  : '~/f/ide/bin/x86_64-linux/fp', },
-  'system': { 'fpc' : '/usr/bin/fpc',
-              'fp'  : '/usr/bin/fp', },
-  'typhon': { 'fpc' : '/usr/lib/codetyphon/fpc/bin/x86_64-linux/fpc',
-              'fp'  : '/usr/lib/codetyphon/fpc/bin/x86_64-linux/fp' },
+              'fp'  : '~/f/ide/bin/%s/fp' % platform, },
+  'system': { 'fpc' : '/usr/local/bin/fpc',
+              'fp'  : '/usr/local/bin/fp', },
+  'typhon': { 'fpc' : '/usr/lib/codetyphon/fpc/bin/%s/fpc' % platform ,
+              'fp'  : '/usr/lib/codetyphon/fpc/bin/%s/fp'  % platform },
 }
 
 # set the ~/bin/* symlinks based on the above paths:
@@ -22,5 +23,5 @@ if len(sys.argv) == 2 and sys.argv[1] in menu:
             symlink = os.sep.join(['~', 'bin', app])
             os.system('rm -f ' + symlink)
             os.system('ln -s {0} {1}'.format(realpath, symlink))
-        else: print "error: {0} does not exist.".format( realpath )
+        else: print "warning: {0} does not exist.".format( realpath )
 else: print "usage : setfpc.py (%s)" % '|'.join(menu.keys())
