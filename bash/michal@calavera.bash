fd60357afb5c243bf10a1a15d4d4163ffafc769e
tangentstorm <michal.wallace@gmail.com>
2015-01-05 01:50:55 -0600

    calavera: fixed path for workshop code

--- bash/michal@calavera.bash
+++ bash/michal@calavera.bash
@@ -1,2 +1,3 @@
 PS1="$BLUE\t$GRAY $WORKD\n$DKRED\u$BRRED@$DKRED\h$RESET:$BLUE$RESET\$ "
-export PYTHONPATH=~/w:~/r/.gen:~/r/code:~/r/lib:~/r/goalpress:~/r/work
+export PYTHONPATH=~/w:~/r/.gen:~/r/code:~/r/lib:~/r/goalpress:~/r/work:~/w/code
+if [ -z "$TMUX" ]; then tmux attach; fi;
