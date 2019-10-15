2e13b5b8a2278a7bb503bd34e2017e8d023da010
tangentstorm <michal.wallace@gmail.com>
2019-10-15 14:10:35 -0400

    emacs: paths for windows vs linux/osx

--- emacs/init.el
+++ emacs/init.el
@@ -1,10 +1,16 @@
 
 ; (package-initialize)
 
+(setq mjw-cfg-root
+      (if (eq system-type 'windows-nt)
+	  "d:/ver/tangentconf/emacs/"
+	"~/t/emacs/"))
+
+
 ; customize should load/save interactive variables from here:
-(setq custom-file "d:/ver/tangentconf/emacs/custom.el")
+(setq custom-file (concat mjw-cfg-root "custom.el"))
 (when (file-exists-p custom-file)
   (load custom-file))
 
 (require 'org)
-(org-babel-load-file "d:/ver/tangentconf/emacs/config.org" 1)
+(org-babel-load-file (concat mjw-cfg-root "config.org") 1)
